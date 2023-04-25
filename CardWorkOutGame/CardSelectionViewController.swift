796f6ae3de0f40b1f13ca58c5bd5b0492585b319
joeyyy688 <edinjoey@gmail.com>
2023-04-25 17:33:27 +0000

    initial commit

---
+++ CardWorkOutGame/CardSelectionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  CardWorkOutGame
+//
+//  Created by Joseph on 4/24/23.
+//
+
+import UIKit
+
+class CardSelectionViewController: UIViewController {
+    
+    
+    @IBOutlet var cardImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func stopButtonOnTap(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func restartButtonOnTap(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func rulesButtonOnTap(_ sender: Any) {
+        
+    }
+    
+    
+}
+
